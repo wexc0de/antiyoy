8196b4b4dad10deeae80e6d3036ec61106e4bdb3
wexc0de <po4olen726@gmail.com>
2026-04-08 23:26:07 +0300

    3

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-async function getWeather() {
-  const response = await fetch('tinkr.tech/sdb/denni_antiyoy/antiyoy');
-  const data = await response.json();
-  console.log(data.temperature);
-}
 async function getData() {
   const response = await fetch('https://tinkr.tech/sdb/denni_antiyoy/antiyoy');
-  const data = await response.json();
-  console.log(data);
+  const state = await response.json();
+  const map = document.getElementById('map');
+
+  for (const hex of state.map) {
+    if (hex.type === 'impassable') continue;
+
+    const img = document.createElement('img');
+    img.src = 'https://tinkr.tech' + hex.image;
+    img.style.position = 'absolute';
+    img.style.left = hex.x + 'px';
+    img.style.top = hex.y + 'px';
+    img.style.width = hex.width + 'px';
+    img.style.height = hex.height + 'px';
+    map.appendChild(img);
+  }
 }
-getData()
+
+getData();
