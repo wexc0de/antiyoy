b19843c67f8ee465143f8c1b59c3c7b7c1670fbe
wexc0de <po4olen726@gmail.com>
2026-04-16 23:33:57 +0300

    4

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ async function getData() {
   for (const hex of state.map) {
     if (hex.type === 'impassable') continue;
 
+    // background tile
     const img = document.createElement('img');
     img.src = 'https://tinkr.tech' + hex.image;
     img.style.position = 'absolute';
@@ -14,6 +15,30 @@ async function getData() {
     img.style.width = hex.width + 'px';
     img.style.height = hex.height + 'px';
     map.appendChild(img);
+
+    // building overlay 
+    if (hex.building_image !== null) {
+      const buildingImg = document.createElement('img');
+      buildingImg.src = 'https://tinkr.tech' + hex.building_image;
+      buildingImg.style.position = 'absolute';
+      buildingImg.style.left = hex.x + 'px';
+      buildingImg.style.top = hex.y + 'px';
+      buildingImg.style.width = hex.width + 'px';
+      buildingImg.style.height = hex.height + 'px';
+      map.appendChild(buildingImg);
+    }
+
+    // unit ovelray
+    if (hex.unit_image !== null) {
+      const unitImg = document.createElement('img');
+      unitImg.src = 'https://tinkr.tech' + hex.unit_image;
+      unitImg.style.position = 'absolute';
+      unitImg.style.left = hex.x + 'px';
+      unitImg.style.top = hex.y + 'px';
+      unitImg.style.width = hex.width + 'px';
+      unitImg.style.height = hex.height + 'px';
+      map.appendChild(unitImg);
+    }
   }
 }
 
